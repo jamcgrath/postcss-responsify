be5ab2fee11e5ca816eedd825137205480dd9331
James McGrath <james@jamesmcgrath.net>
2022-08-05 18:58:26 +0900

    allow to accept multiple selectors

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
  * -[] Support nested @responsive
  */
 
-const postcss = require('postcss');
-const merge = require('lodash/merge');
+const postcss = require("postcss");
+const merge = require("lodash/merge");
 
 /**
  * Processes a breakpoint option by creating a PostCSS @media atRule from the options
@@ -21,29 +21,29 @@ const merge = require('lodash/merge');
  * @return {Object} Processed breakpoint option
  */
 function processBreakpoint(root, breakpointOption) {
-  if (breakpointOption && breakpointOption !== Object(breakpointOption)) {
-    throw new Error('Breakpoint must be of type Object.');
-  }
+	if (breakpointOption && breakpointOption !== Object(breakpointOption)) {
+		throw new Error("Breakpoint must be of type Object.");
+	}
 
-  const processedBreakpoint = Object.assign({}, breakpointOption);
+	const processedBreakpoint = Object.assign({}, breakpointOption);
 
-  let atRule;
-  /* search current rules to see if one exists */
-  root.walkAtRules('media', rule => {
-    if (rule.params !== processedBreakpoint.mediaQuery) return;
-    atRule = rule;
-  });
+	let atRule;
+	/* search current rules to see if one exists */
+	root.walkAtRules("media", (rule) => {
+		if (rule.params !== processedBreakpoint.mediaQuery) return;
+		atRule = rule;
+	});
 
-  /* if no rules exist, create a new one */
-  if (!atRule) {
-    atRule = postcss.atRule({
-      name: 'media',
-      params: processedBreakpoint.mediaQuery,
-    });
-  }
+	/* if no rules exist, create a new one */
+	if (!atRule) {
+		atRule = postcss.atRule({
+			name: "media",
+			params: processedBreakpoint.mediaQuery,
+		});
+	}
 
-  processedBreakpoint.atRule = atRule;
-  return processedBreakpoint;
+	processedBreakpoint.atRule = atRule;
+	return processedBreakpoint;
 }
 
 /**
@@ -53,31 +53,33 @@ function processBreakpoint(root, breakpointOption) {
  * @return {Array} Processed breakpoints option
  */
 function processBreakpoints(root, breakpointsOption) {
-  if (breakpointsOption && !Array.isArray(breakpointsOption)) {
-    throw new Error('Breakpoints option must be of type Array.');
-  }
+	if (breakpointsOption && !Array.isArray(breakpointsOption)) {
+		throw new Error("Breakpoints option must be of type Array.");
+	}
 
-  const defaultBreakpoints = [
-    {
-      prefix: 'xs-',
-      mediaQuery: '(max-width: 40em)',
-    },
-    {
-      prefix: 'sm-',
-      mediaQuery: '(min-width: 40em)',
-    },
-    {
-      prefix: 'md-',
-      mediaQuery: '(min-width: 52em)',
-    },
-    {
-      prefix: 'lg-',
-      mediaQuery: '(min-width: 64em)',
-    },
-  ];
+	const defaultBreakpoints = [
+		{
+			prefix: "xs-",
+			mediaQuery: "(max-width: 40em)",
+		},
+		{
+			prefix: "sm-",
+			mediaQuery: "(min-width: 40em)",
+		},
+		{
+			prefix: "md-",
+			mediaQuery: "(min-width: 52em)",
+		},
+		{
+			prefix: "lg-",
+			mediaQuery: "(min-width: 64em)",
+		},
+	];
 
-  const mergedBreakpoints = merge([], defaultBreakpoints, breakpointsOption);
-  return mergedBreakpoints.map((breakpoint) => processBreakpoint(root, breakpoint));
+	const mergedBreakpoints = merge([], defaultBreakpoints, breakpointsOption);
+	return mergedBreakpoints.map((breakpoint) =>
+		processBreakpoint(root, breakpoint)
+	);
 }
 
 /**
@@ -87,14 +89,22 @@ function processBreakpoints(root, breakpointsOption) {
  * @return {Object} Responsified PostCSS rule
  */
 function createPrefixedRule(rule, prefix) {
-  const prefixLength = prefix.length;
-  const selectorStart = rule.selector.slice(1, prefixLength + 1);
-  const isAlreadyPrefixed = selectorStart === prefix;
-  if (isAlreadyPrefixed) return false;
+	const selectors = rule.selector.replace("\n\t", "").split(",");
+	const prefixLength = prefix.length;
+	const selectorStart = rule.selector.slice(1, prefixLength + 1);
+	const isAlreadyPrefixed = selectorStart === prefix;
 
-  return rule.clone({
-    selector: `.${prefix + rule.selector.substring(1)}`,
-  });
+	if (isAlreadyPrefixed) return false;
+
+	const selectorArray = [];
+	selectors.forEach((s) => {
+		selectorArray.push(`.${prefix + s.substring(1)}`);
+	});
+	const selector = selectorArray.toString();
+
+	return rule.clone({
+		selector,
+	});
 }
 
 /**
@@ -103,27 +113,27 @@ function createPrefixedRule(rule, prefix) {
  * @return {Function}
  */
 function responsifyRule(breakpoints) {
-  /**
-   * Closure function that handles each @responsive sub rule
-   * @param {Object} rule
-   */
-  return (rule) => {
-    /* insert the base rule right before the @responsive rule */
-    const root = rule.parent.parent;
-    const clone = rule.clone();
-    root.insertBefore(rule.parent, clone);
+	/**
+	 * Closure function that handles each @responsive sub rule
+	 * @param {Object} rule
+	 */
+	return (rule) => {
+		/* insert the base rule right before the @responsive rule */
+		const root = rule.parent.parent;
+		const clone = rule.clone();
+		root.insertBefore(rule.parent, clone);
 
-    const isValidSelector = rule.selector.charAt(0) === '.';
-    if (!isValidSelector) return; /* equivilent to continue; */
+		const isValidSelector = rule.selector.charAt(0) === ".";
+		if (!isValidSelector) return; /* equivilent to continue; */
 
-    /* insert each responsive rule in its breakpoint's atRule */
-    breakpoints.forEach((breakpoint) => {
-      const responsiveRule = createPrefixedRule(rule, breakpoint.prefix);
-      if (responsiveRule) {
-        breakpoint.atRule.append(responsiveRule);
-      }
-    });
-  };
+		/* insert each responsive rule in its breakpoint's atRule */
+		breakpoints.forEach((breakpoint) => {
+			const responsiveRule = createPrefixedRule(rule, breakpoint.prefix);
+			if (responsiveRule) {
+				breakpoint.atRule.append(responsiveRule);
+			}
+		});
+	};
 }
 
 /**
@@ -132,28 +142,30 @@ function responsifyRule(breakpoints) {
  * @return {Function}
  */
 function loopResponsiveRules(breakpoints) {
-  /**
-   * Closure function that handles each @responsive atRule
-   * @param {Object} rule
-   */
-  return (rule) => {
-    const subRules = rule.nodes;
-    subRules.forEach(responsifyRule(breakpoints));
-    /* remove the @responsive structure since we've built the responsive rules we need */
-    rule.remove();
-  };
+	/**
+	 * Closure function that handles each @responsive atRule
+	 * @param {Object} rule
+	 */
+	return (rule) => {
+		const subRules = rule.nodes;
+		subRules.forEach(responsifyRule(breakpoints));
+		/* remove the @responsive structure since we've built the responsive rules we need */
+		rule.remove();
+	};
 }
 
-module.exports = postcss.plugin('postcss-responsify', (opts) => {
-  const options = opts || {};
+module.exports = postcss.plugin("postcss-responsify", (opts) => {
+	const options = opts || {};
 
-  return (root) => {
-    const breakpoints = processBreakpoints(root, options.breakpoints);
-    root.walkAtRules('responsive', loopResponsiveRules(breakpoints));
+	return (root) => {
+		const breakpoints = processBreakpoints(root, options.breakpoints);
+		root.walkAtRules("responsive", loopResponsiveRules(breakpoints));
 
-    /* append each breakpoint's populated atRule into the css */
-    root.append(breakpoints.map(breakpoint => {
-      return breakpoint.atRule;
-    }));
-  };
+		/* append each breakpoint's populated atRule into the css */
+		root.append(
+			breakpoints.map((breakpoint) => {
+				return breakpoint.atRule;
+			})
+		);
+	};
 });
